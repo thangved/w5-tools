8ed7084e3a3ec9da36c32769bb84171d87936b33
Kim Minh Thắng <thangved02@gmail.com>
2022-11-29 16:58:10 +0700

    Fix yarn.lock

--- src/pages/Grade/CoursesTable/index.js
+++ src/pages/Grade/CoursesTable/index.js
@@ -1,45 +1,45 @@
-import { DeleteOutlined } from '@ant-design/icons/lib/icons';
-import { Button, Descriptions, Popconfirm, Progress, Space, Table } from 'antd';
-import { useContext, useState } from 'react';
+import { DeleteOutlined } from "@ant-design/icons/lib/icons";
+import { Button, Descriptions, Popconfirm, Progress, Space, Table } from "antd";
+import { useContext, useState } from "react";
 
-import { Grade } from '../../../providers/GradeProvider';
+import { Grade } from "../../../providers/GradeProvider";
 
 const columns = [
 	{
-		title: 'Mã học phần',
-		dataIndex: 'key',
-		key: 'key',
+		title: "Mã học phần",
+		dataIndex: "key",
+		key: "key",
 		width: 150,
-		fixed: 'left',
+		fixed: "left",
 	},
 	{
-		title: 'Tên học phần',
-		dataIndex: 'name',
-		key: 'name',
-		width: 150,
-	},
-	{
-		title: 'Số tín chỉ',
-		dataIndex: 'weight',
-		key: 'weight',
+		title: "Tên học phần",
+		dataIndex: "name",
+		key: "name",
 		width: 150,
 	},
 	{
-		title: 'Điểm thang 10',
-		dataIndex: 'digit',
-		key: 'digit',
+		title: "Số tín chỉ",
+		dataIndex: "weight",
+		key: "weight",
 		width: 150,
 	},
 	{
-		title: 'Điểm thang 4',
-		dataIndex: 'four',
-		key: 'four',
+		title: "Điểm thang 10",
+		dataIndex: "digit",
+		key: "digit",
 		width: 150,
 	},
 	{
-		title: 'Điểm chữ',
-		dataIndex: 'txt',
-		key: 'txt',
+		title: "Điểm thang 4",
+		dataIndex: "four",
+		key: "four",
+		width: 150,
+	},
+	{
+		title: "Điểm chữ",
+		dataIndex: "txt",
+		key: "txt",
 		with: 150,
 	},
 ];
@@ -52,9 +52,10 @@ const CoursesTable = () => {
 		<Space
 			direction="vertical"
 			style={{
-				maxWidth: 'calc(100vw - 100px)',
-				overflow: 'auto',
-			}}>
+				maxWidth: "calc(100vw - 100px)",
+				overflow: "auto",
+			}}
+		>
 			<Popconfirm
 				title={`Bạn chắc chắn muốn xóa ${selected.length} học phần?`}
 				disabled={selected.length === 0 || courses.length === 0}
@@ -81,44 +82,38 @@ const CoursesTable = () => {
 				pagination={{ pageSize: 9 }}
 				dataSource={courses}
 				columns={columns}
-				scroll={{ x: 900, y: '100vh' }}
+				scroll={{ x: 900, y: "100vh" }}
 				id="course-table"
 			/>
-			<Descriptions
-				layout="vertical"
-				bordered>
-				<Descriptions.Item
-					span={4}
-					key="avg"
-					label="Điểm trung bình">
+			<Descriptions layout="vertical" bordered>
+				<Descriptions.Item span={4} key="avg" label="Điểm trung bình">
 					<Progress
 						strokeColor={{
-							'0%': '#108ee9',
-							'100%': '#87d068',
+							"0%": "#108ee9",
+							"100%": "#87d068",
 						}}
 						type="circle"
-						percent={avg * 100 / 4}
-						format={percent => {
-							return `${percent * 4 / 100}/4`
+						percent={(avg * 100) / 4}
+						format={(percent) => {
+							return `${(percent * 4) / 100}/4`;
 						}}
 					/>
 				</Descriptions.Item>
-				{
-					courses.map(course => (
-						<Descriptions.Item
-							span={1}
-							key={course.key}
-							label={course.name}>
-							<Progress
-								type="circle"
-								percent={course.four * 100 / 4}
-								format={percent => {
-									return `${percent * 4 / 100}/4`
-								}}
-							/>
-						</Descriptions.Item>
-					))
-				}
+				{courses.map((course) => (
+					<Descriptions.Item
+						span={1}
+						key={course.key}
+						label={course.name}
+					>
+						<Progress
+							type="circle"
+							percent={(course.four * 100) / 4}
+							format={(percent) => {
+								return `${(percent * 4) / 100}/4`;
+							}}
+						/>
+					</Descriptions.Item>
+				))}
 			</Descriptions>
 		</Space>
 	);
